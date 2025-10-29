a020bf22e590560d8841d97b893680595042a0f3
hakimnazry24 <hakimnazry@gmail.com>
2025-10-29 11:52:05 +0800

    remove: action button

--- app/content_scripts/shopee.js
+++ app/content_scripts/shopee.js
@@ -1,5 +1,39 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    if (msg.action === "filterItem") {
-        
+async function main() {
+  window.addEventListener("scroll", async () => {
+    await runFilter();
+  });
+}
+
+async function runFilter() {
+  const boycottedBrands = await _fetchBoycottItemList();
+
+  const listItems = Array.from(
+    document.querySelectorAll("ul .shopee-search-item-result__item")
+  );
+
+  listItems.forEach((li) => {
+    const itemEl = li.querySelector(".line-clamp-2");
+    if (!itemEl) return;
+
+    const itemName = itemEl.textContent.toLowerCase().trim();
+
+    const matched = boycottedBrands.some((brand) => itemName.includes(brand));
+
+    if (matched) {
+      li.style.pointerEvents = "none";
+      li.style.opacity = 0.01;
     }
-})
+  });
+}
+async function _fetchBoycottItemList() {
+  const url = chrome.runtime.getURL("boycott_list.txt");
+  const res = await fetch(url);
+  const text = await res.text();
+
+  return text
+    .split("\n")
+    .map((line) => line.toLowerCase().trim())
+    .filter(Boolean);
+}
+
+main();
--- config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    "filePath": "boycott_list.txt",
-    "websiteList": ["shopee", "lazada"]
-}
-
-export default config
